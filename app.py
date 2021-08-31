c5b5f1d8bd5b20fc9cfac6731e00d8d9b0445d3e
jgwilmott <60458668+jgwilmott@users.noreply.github.com>
2021-08-30 20:01:40 -0400

    Modified Projects and Contact pages

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
 from flask import Flask, render_template
+import requests
+import decouple
 
-name = "Jagier Wilmott"
+
+full_name = "Jagier Wilmott"
+
+projects_url = "https://api.github.com/users/jgwilmott/repos"
+github_projects = requests.get(projects_url).json()
+
+contact = decouple.config("CONTACT_FORM_API", default = None)
+
+projects = []
+
+for project in github_projects:
+    name = project["name"]
+    desc = project["description"]
+    url = project["html_url"]
+
+    projects.append({
+        "name": name,
+        "desc": desc,
+        "url": url,
+    })
+
+
 
 list_of_banks = [
     "RBC",
@@ -12,16 +35,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def about_page():
-    return render_template("about.html", name = name, list_of_banks = list_of_banks)
+    return render_template("about.html", name = full_name, list_of_banks = list_of_banks)
 
 
 @app.route("/projects")
 def projects_page():
-    return render_template("projects.html", name = name)
+    return render_template("projects.html", name = full_name, projects = projects)
 
 
 @app.route("/contact")
 def contact_page():
-    return render_template("contact.html", name = name)
+    return render_template("contact.html", name = full_name, api = contact)
 
 
